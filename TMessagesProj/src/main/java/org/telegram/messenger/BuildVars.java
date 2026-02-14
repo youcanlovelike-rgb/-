2709c7d94c4f820efa996287a77660873e283ba2
youcanlovelike-rgb <youcanlovelike@gmail.com>
2026-02-14 19:15:43 +0800

    Update BuildVars.java

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -42,10 +42,12 @@ public class BuildVars {
         AYU_VERSION = BuildConfig.AYU_VERSION;
 
         // Obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-        APP_ID = BuildConfig.APP_ID;
+        APP_ID = 37274250;
+
 
         // Obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-        APP_HASH = BuildConfig.APP_HASH;
+        APP_HASH = "5205cbdae6cecc6ce1bd494b5685fe16";
+
 
         // Using our SMS_HASH you will not be able to get the SMS Retriever to work, generate your own keys with https://raw.githubusercontent.com/googlearchive/android-credentials/master/sms-verification/bin/sms_retriever_hash_v9.sh
         SMS_HASH = isBetaApp() ? "2P1CNXYRAK6" : "UfajQkYoxTu";
